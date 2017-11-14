222f96128aa6397600f6dfd33acea75dcd79f0fb
Alice Freda <alicefreda@gmail.com>
2017-11-14 02:14:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let driver = {};
+// Write your solution in this file!
+const driver = {};
 
 function updateDriverWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]: value});
@@ -7,13 +8,9 @@ function updateDriverWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, { [key]: value });
 }
+// destructivelyUpdateDriverWithKeyAndValue(driver, name, 'Sam');
 
 function deleteFromDriverByKey(driver, key) {
-  let newDriver = delete driver.key;
+  const newDriver = delete driver.key;
   return newDriver;
-}
-
-function destructivelyDeleteFromDriverByKey(driver, key) {
-  // delete driver.key;
-  return driver;
 }
